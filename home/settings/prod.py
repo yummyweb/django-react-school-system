38f360eb98ecf3b9941053f61fcf3f0e34b388fb
antriksh123 <antariksha.verma@pathways.in>
2020-12-13 18:12:53 +0530

    Initial Commit

---
+++ home/settings/prod.py
@@ -0,0 +1,27 @@
+'''Use this for production'''
+
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS += ['http://domain.com']
+WSGI_APPLICATION = 'home.wsgi.prod.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '2aad29adcb34c11b56b30c78511815ca',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
